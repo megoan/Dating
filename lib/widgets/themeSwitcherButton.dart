77a74ace3609acffd83ba354bfc81bf8cba981e5
megoan <shmuelsoib@gmail.com>
2019-10-01 22:33:29 +0300

    fixed theme change

--- lib/widgets/themeSwitcherButton.dart
+++ lib/widgets/themeSwitcherButton.dart
@@ -22,42 +22,7 @@ class _FancyFabState extends State<FancyFab> with SingleTickerProviderStateMixin
   Curve _curve = Curves.easeOut;
   HighLightedIcon selectedIcon;
   bool set=false;
-  @override
-  void didChangeDependencies() {
-    if(!set){
-      if(Theme.of(context).brightness==Brightness.light){
-        isOpened= false;
-        _animateColor = ColorTween(
-      begin: Color.fromRGBO(16, 0, 38, 1),
-      end: Colors.white,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Interval(
-        0.00,
-        1.00,
-        curve: _curve,
-      ),
-    ));
-      }
-      else{
-        isOpened =true;
-           _animateColor = ColorTween(
-      end: Color.fromRGBO(16, 0, 38, 1),
-      begin: Colors.white,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Interval(
-        0.00,
-        1.00,
-        curve: _curve,
-      ),
-    ));
-      }
-      set=true;
-    }
-   
-    super.didChangeDependencies();
-  }
+
 
   @override
   initState() {
@@ -77,41 +42,71 @@ class _FancyFabState extends State<FancyFab> with SingleTickerProviderStateMixin
   }
 
   animate() {
-    
-    if (!isOpened) {
-    
-      _animationController.forward();
-
-    } else {
-     
-      _animationController.reverse();
-    }
-     ThemeController.switchTheme(context);
-   
-    setState(() {
-       isOpened = !isOpened;
-    });
-     
+     ThemeController.switchTheme(context);    
   }
 
   Widget toggle() {
+     _animationController.forward();
+    if(Theme.of(context).brightness!=Brightness.light){
+      
+    _animateColor = ColorTween(
+      begin: Color.fromRGBO(16, 0, 38, 1),
+      end: Colors.white,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: _curve,
+      ),
+    ));
+    }
+    else{
+    
+ _animateColor = ColorTween(
+      end: Color.fromRGBO(16, 0, 38, 1),
+      begin: Colors.white,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: _curve,
+      ),
+    ));
+    }
     return FloatingActionButton(
-      backgroundColor: _animateColor.value,
+      backgroundColor: (Theme.of(context).brightness==Brightness.light)?ColorTween(
+      end: Color.fromRGBO(16, 0, 38, 1),
+      begin: Colors.white,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: _curve,
+      ),
+    )).value:ColorTween(
+      begin: Color.fromRGBO(16, 0, 38, 1),
+      end: Colors.white,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: _curve,
+      ),
+    )).value,
       onPressed: animate,
       tooltip: 'Toggle',
-      child: (isOpened)? HighLightedIcon(Icons.wb_sunny,color: Theme.of(context).accentColor,):
+      child: (Theme.of(context).brightness!=Brightness.light)? HighLightedIcon(Icons.wb_sunny,color: Theme.of(context).accentColor,):
       HighLightedIcon(Icons.brightness_3,color: Theme.of(context).accentColor,),
-      // child: AnimatedIcon(
-      //   icon: AnimatedIcons.menu_home,
-      //   color:Colors.red,
-      //   progress: _animateIcon,
-      // ),
+
     );
   }
 
   @override
   Widget build(BuildContext context) {
-  
     return toggle();
   }
 }
