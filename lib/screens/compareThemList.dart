7397f9405f65832870bb9ee730c7143476a35b12
shmuelsoib <54931488+shmuelsoib@users.noreply.github.com>
2019-11-26 20:45:21 +0200

    change

--- lib/screens/compareThemList.dart
+++ lib/screens/compareThemList.dart
@@ -1,4 +1,5 @@
 import 'package:dating/providers/langText.dart';
+import 'package:dating/widgets/compareCard.dart';
 import 'package:flutter/material.dart';
 
 import '../main.dart';
@@ -8,7 +9,10 @@ class CompareThemList extends StatefulWidget {
 }
 
 class _CompareThemListState extends State<CompareThemList> {
-
+   Widget returnCard(BuildContext ctxt, int index){
+    return new CompareCard();
+  }
+ List<String> litems = ["1","2","Third","4"];
   bool allSelected = true;
   bool myListSelected = false;
   bool myFavoritesSelected = false;
@@ -42,117 +46,131 @@ class _CompareThemListState extends State<CompareThemList> {
       body: Container(
         child: Column(
           children: <Widget>[
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-              if(myListSelected)  RaisedButton(onPressed: (){
-                // setState(() {
-                //      myListSelected=!myListSelected;       
-                // });
-                 
-              },
-                color: Colors.purple,
-                child: Text(LocaleText.getLocaleText(MyApp.getLocale(), 'My list'),style:TextStyle(color: Colors.white)),
+            Container(
+              color: Colors.grey[200],
+              child: Row(
                 
-                 shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))),
-              if(!myListSelected) OutlineButton( onPressed: (){   
-                setState(() {
-                     myListSelected=!myListSelected;       
-                     myFavoritesSelected = false;
-                     allSelected = false;
-                });   
-                      },
-                    child: Container(
-                      child: Row(
-                      
-                        children: <Widget>[
-                          Text(LocaleText.getLocaleText(MyApp.getLocale(), 'My list'),style: TextStyle(color: Colors.purple),),
-                        ],
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                if(myListSelected)  RaisedButton(onPressed: (){
+                  // setState(() {
+                  //      myListSelected=!myListSelected;       
+                  // });
+                   
+                },
+                  color: Colors.purple,
+                  child: Text(LocaleText.getLocaleText(MyApp.getLocale(), 'My list'),style:TextStyle(color: Colors.white)),
+                  
+                   shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))),
+                if(!myListSelected) OutlineButton( onPressed: (){   
+                  setState(() {
+                       myListSelected=!myListSelected;       
+                       myFavoritesSelected = false;
+                       allSelected = false;
+                  });   
+                        },
+                      child: Container(
+                        child: Row(
+                        
+                          children: <Widget>[
+                            Text(LocaleText.getLocaleText(MyApp.getLocale(), 'My list'),style: TextStyle(color: Colors.purple),),
+                          ],
+                        ),
                       ),
-                    ),
-                    shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(30.0),
-                            ),
-                   borderSide: BorderSide(
-                     
-                color: Colors.purple, //Color of the border
-                style: BorderStyle.solid, //Style of the border
-                width: 1, //width of the border
-              ),),
-              if(myFavoritesSelected)  RaisedButton(onPressed: (){
-                // setState(() {
-                //    myFavoritesSelected=!myFavoritesSelected;     
-                // });
-                 
-              },
-                color: Colors.purple,
-                child: Text(LocaleText.getLocaleText(MyApp.getLocale(), 'Favorites'),style:TextStyle(color: Colors.white)),
+                      shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(30.0),
+                              ),
+                     borderSide: BorderSide(
+                       
+                  color: Colors.purple, //Color of the border
+                  style: BorderStyle.solid, //Style of the border
+                  width: 1, //width of the border
+                ),),
+                if(myFavoritesSelected)  RaisedButton(onPressed: (){
+                  // setState(() {
+                  //    myFavoritesSelected=!myFavoritesSelected;     
+                  // });
+                   
+                },
+                  color: Colors.purple,
+                  child: Text(LocaleText.getLocaleText(MyApp.getLocale(), 'Favorites'),style:TextStyle(color: Colors.white)),
+                  
+                   shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))),
+                if(!myFavoritesSelected) OutlineButton( onPressed: (){ 
+                  setState(() {
+                        myFavoritesSelected=!myFavoritesSelected;     
+                        myListSelected =false;
+                        allSelected = false;   
+                  });   
+               
+                        },
+                      child: Container(
+                        child: Row(
+                        
+                          children: <Widget>[
+                            Text(LocaleText.getLocaleText(MyApp.getLocale(), 'Favorites'),style: TextStyle(color: Colors.purple),),
+                          ],
+                        ),
+                      ),
+                      shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(30.0),
+                              ),
+                     borderSide: BorderSide(
+                       
+                  color: Colors.purple, //Color of the border
+                  style: BorderStyle.solid, //Style of the border
+                  width: 1, //width of the border
+                ),),
+                 if(allSelected)  RaisedButton(onPressed: (){
+                  // setState(() {
+                  //    allSelected=!allSelected;     
+                  // });
+                   
+                },
+                  color: Colors.purple,
+                  child: Text(LocaleText.getLocaleText(MyApp.getLocale(), 'Everyone'),style:TextStyle(color: Colors.white)),
+                  
+                   shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))),
+                if(!allSelected) OutlineButton( onPressed: (){   
+                  setState(() {
+                      allSelected=!allSelected; 
+                      myFavoritesSelected= false;
+                      myListSelected= false;      
+                  });   
                 
-                 shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))),
-              if(!myFavoritesSelected) OutlineButton( onPressed: (){ 
-                setState(() {
-                      myFavoritesSelected=!myFavoritesSelected;     
-                      myListSelected =false;
-                      allSelected = false;   
-                });   
-             
-                      },
-                    child: Container(
-                      child: Row(
-                      
-                        children: <Widget>[
-                          Text(LocaleText.getLocaleText(MyApp.getLocale(), 'Favorites'),style: TextStyle(color: Colors.purple),),
-                        ],
+                        },
+                      child: Container(
+                        child: Row(
+                        
+                          children: <Widget>[
+                            Text(LocaleText.getLocaleText(MyApp.getLocale(), 'Everyone'),style: TextStyle(color: Colors.purple),),
+                          ],
+                        ),
                       ),
-                    ),
-                    shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(30.0),
-                            ),
-                   borderSide: BorderSide(
-                     
-                color: Colors.purple, //Color of the border
-                style: BorderStyle.solid, //Style of the border
-                width: 1, //width of the border
-              ),),
-               if(allSelected)  RaisedButton(onPressed: (){
-                // setState(() {
-                //    allSelected=!allSelected;     
-                // });
-                 
-              },
-                color: Colors.purple,
-                child: Text(LocaleText.getLocaleText(MyApp.getLocale(), 'Everyone'),style:TextStyle(color: Colors.white)),
+                      shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(30.0),
+                              ),
+                     borderSide: BorderSide(
+                       
+                  color: Colors.purple, //Color of the border
+                  style: BorderStyle.solid, //Style of the border
+                  width: 1, //width of the border
+                ),),
                 
-                 shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))),
-              if(!allSelected) OutlineButton( onPressed: (){   
-                setState(() {
-                    allSelected=!allSelected; 
-                    myFavoritesSelected= false;
-                    myListSelected= false;      
-                });   
-              
-                      },
-                    child: Container(
-                      child: Row(
-                      
-                        children: <Widget>[
-                          Text(LocaleText.getLocaleText(MyApp.getLocale(), 'Everyone'),style: TextStyle(color: Colors.purple),),
-                        ],
-                      ),
-                    ),
-                    shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(30.0),
-                            ),
-                   borderSide: BorderSide(
-                     
-                color: Colors.purple, //Color of the border
-                style: BorderStyle.solid, //Style of the border
-                width: 1, //width of the border
-              ),),
-              
-              ],
-            )
+                ],
+              ),
+            ),
+          Expanded(
+            child: ListView.builder(
+  itemCount: litems.length,
+  itemBuilder: (BuildContext ctxt, int index) => Padding(
+    padding: const EdgeInsets.all(0.0),
+    child: returnCard(ctxt, index),
+  )
+),
+          )
           ],
+       
         ),
       ),
     );
--- lib/widgets/compareCard.dart
+++ lib/widgets/compareCard.dart
@@ -7,96 +7,90 @@ import 'loader.dart';
 
 class CompareCard extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-  return Card(
+ Widget build(BuildContext context) {
+    return Card(
           child: Container(
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-          
-               Padding(
-                 padding: const EdgeInsets.all(8.0),
-                 child: Stack(
-                                      alignment: AlignmentDirectional.center,
-                                      children: <Widget>[
-                                        Container(
-                                          width: 70,
-                                          height: 70,
-                                          child: CircleAvatar(
-                                            radius: 70,
-                                            backgroundColor:
-                                                Theme.of(context).primaryColor,
-                                          ),
-                                        ),
-                                        Container(
-                                          width:65,
-                                          height: 65,
-                                          child: ClipOval(
-                                            child: CachedNetworkImage(
-                                              fit: BoxFit.cover,
-                                              imageUrl:
-                                                  'https://placeimg.com/640/480/any',
-                                              placeholder: (context, url) => Loader(),
-                                              errorWidget: (context, url, error) =>
-                                                  Icon(Icons.error),
-                                            ),
-                                          ),
-                                        ),
-                                       
-                                      ],
-                                    ),
-               ),
-               ClipRRect(
-                 borderRadius: (BorderRadius.circular(10)),
-                                child: Container(
-                   color: Colors.blueAccent,
-                           
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text("98%",style: TextStyle(color: Colors.white,fontSize: 20),),
+            Padding(
+              padding: MyApp.getLocale()=="he"? const EdgeInsets.only(right: 20):const EdgeInsets.only(left: 20),
+              child:new Container(
+                width: 70,
+                height: 70,
+          padding: EdgeInsets.all(1),
+          decoration: new BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(35),
+          ),
+          constraints: BoxConstraints(
+            minWidth: 12,
+            minHeight: 12,
+          ),
+          child: Align(
+            alignment: Alignment.center,
+                      child: new Text(
+              '66%',
+              style: new TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+            ),
+             Padding(
+                        padding:   MyApp.getLocale()=="he"? const EdgeInsets.only(right: 40,left: 15,top: 15,bottom: 15):const EdgeInsets.only(left: 40,right: 15,top: 15,bottom: 15),
+                        child: Stack(
+                          alignment: AlignmentDirectional.center,
+                          children: <Widget>[
+                            Container(
+                              width: 70,
+                              height: 70,
+                              child: ClipOval(
+                                child: CachedNetworkImage(
+                                  fit: BoxFit.cover,
+                                  imageUrl: 'https://placeimg.com/640/480/any',
+                                  placeholder: (context, url) => Loader(),
+                                  errorWidget: (context, url, error) =>
+                                      Icon(Icons.error),
+                                ),
                               ),
-                             
-                           
+                            ),
+                          ],
+                        ),
+                      ),
+                       Expanded(
+                                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Text( "Samy", style:   TextStyle(fontSize: 18, fontWeight: FontWeight.bold),  ),
+                                     Text( " ,", style:   TextStyle(fontSize: 18, fontWeight: FontWeight.bold),  ),
+                                      Text( "25", style:   TextStyle(fontSize: 18, fontWeight: FontWeight.bold),  ),
+                                  ],
+                                ),
+                                 Row(
+                                   children: <Widget>[
+                                     Text("Bet Shemesh",),
+                                     Icon(Icons.location_on,color:Colors.purple),
+                                   ],
+                                 ),
+                              
+                              ],
+                            ),
                           ),
-               ),
-                     
-                 Padding(
-                   padding: const EdgeInsets.all(8.0),
-                   child: Stack(
-                                      alignment: AlignmentDirectional.center,
-                                      children: <Widget>[
-                                        Container(
-                                          width: 70,
-                                          height: 70,
-                                          child: CircleAvatar(
-                                            radius: 70,
-                                            backgroundColor:
-                                                Theme.of(context).primaryColor,
-                                          ),
-                                        ),
-                                        Container(
-                                          width:65,
-                                          height: 65,
-                                          child: ClipOval(
-                                            child: CachedNetworkImage(
-                                              fit: BoxFit.cover,
-                                              imageUrl:
-                                                  'https://placeimg.com/640/480/any',
-                                              placeholder: (context, url) => Loader(),
-                                              errorWidget: (context, url, error) =>
-                                                  Icon(Icons.error),
-                                            ),
-                                          ),
-                                        ),
-                                       
-                                      ],
-                                    ),
-                 ),
+                        ),
+                      ),
+          
           ],
           
         ),
       ),
     );
-  
   }
 }
